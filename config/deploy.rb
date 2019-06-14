ea28cccf440b94bc5d58dea10ec48fe1011588b1
kobayashi <kortaban@gmail.com>
2019-06-14 20:49:27 +0900

    carrierwave

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,6 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
-
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,11 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
+    
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1' #例 'ap-northeast-1'
+    region: 'ap-northeast-1'
   }
-
   config.fog_directory  = 'kobayashi333'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kobayashi333'
 end
